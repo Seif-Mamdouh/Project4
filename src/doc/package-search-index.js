c1b539e45cf3dcdf28ab8529636132a0c6a54dd5
Seif Mamdouh <seifmamdouh@Seifs-MacBook-Pro-2.local>
2024-10-01 13:33:55 -0400

    add console log statement

--- src/doc/package-search-index.js
+++ src/doc/package-search-index.js
@@ -1,1 +1,3 @@
 packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.project4","l":"com.example.project4"},{"m":"com.example.project4","l":"com.example.project4.RUpizza"}];updateSearchResults();
+
+console.log("Hello");
